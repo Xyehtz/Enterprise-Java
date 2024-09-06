dcc3047245d85a2726b74381d6d9ed677d666f28
Xyehtz <alejitogarbo@gmail.com>
2024-09-06 16:12:14 -0400

    Template ICE1

---
+++ ICE1/src/main/java/ca/sheridancollege/garzonba/Ice1Application.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.garzonba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ice1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ice1Application.class, args);
+    }
+
+}
---
+++ ICE1/src/test/java/ca/sheridancollege/garzonba/Ice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.garzonba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ice1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
