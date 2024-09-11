60e6e3983d441f639b1b8daa81d5e201e854ace9
Xyehtz <alejitogarbo@gmail.com>
2024-09-11 08:54:36 -0400

    week 2 day 2

---
+++ Week 2 Day 2/src/main/java/ca/sheridancollege/garzonba/week2day2/Week2Day2Application.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.garzonba.week2day2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2Day2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week2Day2Application.class, args);
+    }
+
+}
---
+++ Week 2 Day 2/src/test/java/ca/sheridancollege/garzonba/week2day2/Week2Day2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.garzonba.week2day2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2Day2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
