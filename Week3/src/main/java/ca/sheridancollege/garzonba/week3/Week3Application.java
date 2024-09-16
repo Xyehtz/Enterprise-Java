2d57d277ffe3ba44f8c779a69e22de20776354d7
Alejandro Garzon <alejitogarbo@gmail.com>
2024-09-16 09:28:27 -0400

    Created simple Lombok project

--- Week3/src/main/java/ca/sheridancollege/garzonba/week3/Week3Application.java
+++ Week3/src/main/java/ca/sheridancollege/garzonba/week3/Week3Application.java
@@ -1,13 +1,23 @@
 package ca.sheridancollege.garzonba.week3;
 
+import ca.sheridancollege.garzonba.week3.beans.Inventory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Week3Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Week3Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Week3Application.class, args);
+		
+		Inventory inventory = Inventory.builder().itemId(32L).itemName("Pencil").quantity(5).build();
+
+//		Inventory inv1 = new Inventory();
+//		inv1.setItemId(2344L);
+//		inv1.setItemName("Car");
+//		inv1.setQuantity(3);
+//		
+//		Inventory inv2 = new Inventory(242L, "Nanobot", 4)
+	}
 
 }
---
+++ Week3/src/main/java/ca/sheridancollege/garzonba/week3/beans/Inventory.java
@@ -0,0 +1,20 @@
+package ca.sheridancollege.garzonba.week3.beans;
+
+import lombok;
+
+@Data
+@Builder
+public class Inventory {
+    private long itemID;
+    private String itemName;
+    private int quantity;
+}
+
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class Inventory {
+//    private long itemId;
+//    private String itemName;
+//    private int quantity;
+//}
