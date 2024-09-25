1eccf17f21ff29c56af206042bf629673a4c120a
Alejandro Garzon <alejitogarbo@gmail.com>
2024-09-25 11:53:34 -0400

    ice4 init

---
+++ ex4_thymeleaf/src/test/java/ca/sheridancollege/garzonba/ex4_thymeleaf/Ex4ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.garzonba.ex4_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex4ThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
