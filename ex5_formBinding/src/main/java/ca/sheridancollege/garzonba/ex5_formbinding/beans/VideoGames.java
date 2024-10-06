3c5c8564b667408f689a0c04ac5149625e099eb5
Alejandro Garzon <alejitogarbo@gmail.com>
2024-10-06 13:55:03 -0400

    created VideoGames bean

--- ex5_formBinding/src/main/java/ca/sheridancollege/garzonba/ex5_formbinding/beans/VideoGames.java
+++ ex5_formBinding/src/main/java/ca/sheridancollege/garzonba/ex5_formbinding/beans/VideoGames.java
@@ -1,4 +1,18 @@
 package ca.sheridancollege.garzonba.ex5_formbinding.beans;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class VideoGames {
+    private Long id;
+    private String title;
+    private String publisher;
+    private String platform;
+    private BigDecimal price;
 }
