444f631faac58c48cffabac419e6e8408275e282
Alejandro Garzon <alejitogarbo@gmail.com>
2024-10-10 16:06:02 -0400

    created mission bean

---
+++ Assignment2/src/main/java/ca/sheridancollege/garzonba/assignment2/beans/Mission.java
@@ -0,0 +1,10 @@
+package ca.sheridancollege.garzonba.assignment2.beans;
+
+public class Mission {
+    private int id;
+    private String title;
+    private String gadget1;
+    private String gadget2;
+    private String agent;
+    private final String[] agents = {"Johnny English", "Natasha Romanova", "Austin Powers"};
+}
